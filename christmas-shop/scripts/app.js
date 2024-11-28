d54020c1b03820ac2b094a16c70898c53e798add
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2024-11-28 15:23:21 +0100

    refactor: add variable for overlay

--- christmas-shop/scripts/app.js
+++ christmas-shop/scripts/app.js
@@ -1,6 +1,7 @@
 const resources = fetch('../res/gifts.json').then(res => res.json());
 const isGiftsPage = window.location.pathname.includes('/gifts/');
 
+const body = document.querySelector('#top');
 const indicatorsArray = document.querySelectorAll('.cta__timer__indicator > h2');
 const christmasButton = document.querySelectorAll('.christmas__button');
 const menuLinksArea = document.querySelector('.menu__links');
