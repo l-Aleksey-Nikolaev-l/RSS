faf02fd9536fa34f28bfb38e66bbec826a084ded
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2024-11-29 21:49:17 +0100

    refactor: add function with svg

--- christmas-shop/scripts/app.js
+++ christmas-shop/scripts/app.js
@@ -26,6 +26,25 @@ class Card {
         this.superpowers = superpowers;
     }
 
+    #getStar() {
+        const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+        const path = document.createElementNS("http://www.w3.org/2000/svg", 'path');
+        svg.setAttribute('viewBox', '0 0 20.7 24');
+        path.setAttribute('d', 'M17.3,14.8l-.8-.5,2.2-.6-.4-1.4-3.6,1-1.7-1v-.8l1.7-1,3.6,1,' +
+            '.4-1.4-2.2-.6.8-.5h3.5l.5-3L18.5,5,16.6,8l-.8.5.6-2.2L15,5.9,14,9.5l' +
+            '-1.7,1a1.61,1.61,0,0,0-.7-.4v-2l2.6-2.6-1-1L11.6,6.1v-1L13.3,2,11,0,' +
+            '8.7,2l1.6,3.1V6L8.7,4.5l-1,1,2.6,2.6V10a2.51,2.51,0,0,0-.7.4L8,9.4,7,' +
+            '5.9l-1.4.3.6,2.2L5.4,8,3.5,5,.7,6l.5,3,3.5.2.8.5-2.2.6.4,1.4,3.6-1,1.7,' +
+            '1v.8l-1.7,1-3.6-1-.4,1.4,2.2.6-.8.5H1.2L.7,18l2.8,1,1.9-3,.8-.5-.6,2.2,' +
+            '1.4.4,1-3.6,1.7-1a1.61,1.61,0,0,0,.7.4v1.9L7.8,18.4l1,1,1.6-1.6v.9L8.7,' +
+            '22,11,24l2.3-2-1.6-3.1V18l1.6,1.6,1-1L11.7,16V14a2.51,2.51,0,0,0,.7-.4' +
+            'l1.7,1,1,3.6,1.4-.4-.6-2.2.8.5L18.6,19l2.8-1-.6-3Z');
+        path.setAttribute('transform', 'translate(-0.7)');
+        svg.appendChild(path);
+        svg.setAttribute('fill', 'var(--primary-color-10)');
+        return svg;
+    }
+
     #createAdds(adds) {
         let newAdds = '';
         for (let [superpower, score] of Object.entries(adds)) {
