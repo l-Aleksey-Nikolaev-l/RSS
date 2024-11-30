184e477ff65c7b01b4f668d9b2efa9feb3594e94
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2024-11-30 15:16:59 +0100

    refactor: chane unused variables to imports

--- christmas-shop/scripts/app.js
+++ christmas-shop/scripts/app.js
@@ -1,23 +1,6 @@
-const resources = fetch('./res/gifts.json').then(res => res.json());
-const isGiftsPage = window.location.pathname.includes('/gifts/');
-
-const body = document.querySelector('#top');
-const indicatorsArray = document.querySelectorAll('.cta__timer__indicator > h2');
-const christmasButton = document.querySelectorAll('.christmas__button');
-const burgerButton = document.querySelector('#burger__checkbox');
-const menuLinksArea = document.querySelector('.menu__links');
-const slider = document.querySelector('.slider__carousel');
-const carousel = document.querySelector('.carousel__list');
-const sliderButtons = document.querySelectorAll('.slider__button');
-const giftsSection = document.querySelector('.gift__cards');
-const filterTabs = document.querySelector(".mg--tabs");
-const backToTop = document.querySelector('.back--to--top');
-let closeModalButton = document.querySelector('.gift__card__close');
-
-let carouselPosition = 0;
-
-let activeTab = 'all';
-
+import addRandomCards from './cards.js';
+import showTimeToNewYear from './timer.js';
+import startListeners from './listeners.js'
 
 
 addRandomCards();
