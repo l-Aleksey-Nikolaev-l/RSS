2a40c661a8bcb808641f54f27f995a7b19e39fea
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2024-11-30 15:19:04 +0100

    refactor: move all listeners to their own module listeners

--- christmas-shop/scripts/app.js
+++ christmas-shop/scripts/app.js
@@ -6,32 +6,3 @@ import startListeners from './listeners.js'
 addRandomCards();
 showTimeToNewYear();
 setInterval(showTimeToNewYear, 1000);
-
-christmasButton.forEach((button) => {
-    button.addEventListener('click', () => {
-        location.href = './gifts';
-    })
-});
-
-window.addEventListener('scroll', defineScrollPosition);
-
-menuLinksArea.addEventListener('click', () => {
-    burgerButton.checked = false;
-})
-
-window.addEventListener('resize', () => {
-    resetSlider();
-    defineScrollPosition();
-    closeMobileMenu();
-});
-
-sliderButtons.forEach((button) => {
-    button.addEventListener('click', clickSliderButton)
-});
-
-filterTabs?.addEventListener('click', switchTab);
-
-giftsSection.addEventListener('click', showModal);
-
-backToTop?.addEventListener('click',() => {
-});
