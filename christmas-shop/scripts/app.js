31de6ff31677a90410ff4079859eaf8386d5474a
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2024-11-30 15:12:20 +0100

    refactor: move slider functions to own slider module

--- christmas-shop/scripts/app.js
+++ christmas-shop/scripts/app.js
@@ -18,36 +18,6 @@ let carouselPosition = 0;
 
 let activeTab = 'all';
 
-function resetSlider() {
-    if (!isGiftsPage) {
-        carouselPosition = 0;
-        carousel.style.left = carouselPosition;
-        sliderButtons[0].classList.add('slider__button__disabled');
-        sliderButtons[1].classList.remove('slider__button__disabled');
-    }
-}
-
-function clickSliderButton(button) {
-    const carouselDivider = window.innerWidth <= 768 ? 6 : 3;
-    const carouselTail = carousel.offsetWidth - slider.offsetWidth;
-    const carouselDividedTail = Math.round(carouselTail / carouselDivider);
-    const tolerance = 3;
-    const direction = button.currentTarget.dataset.direction;
-    if (direction === 'next') {
-        sliderButtons[0].classList.remove('slider__button__disabled');
-        carouselPosition -= carouselDividedTail;
-        if(Math.abs(carouselPosition) >= carouselTail - tolerance) {
-            button.currentTarget.classList.toggle('slider__button__disabled');
-        }
-    } else {
-        sliderButtons[1].classList.remove('slider__button__disabled');
-        carouselPosition += carouselDividedTail;
-        if(Math.abs(carouselPosition) <= 0) {
-            button.currentTarget.classList.toggle('slider__button__disabled');
-        }
-    }
-    carousel.style.left = carouselPosition + 'px';
-}
 
 function switchTab(event) {
     if(!event.target.childElementCount) {
