80439ab48c231ab8f780ee422a8ef0193c18ec90
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2025-01-07 15:04:16 +0100

    refactor: add onload to body for display main screen

--- Simon-Says/scripts/app.js
+++ Simon-Says/scripts/app.js
@@ -1,6 +1,11 @@
-import {body} from "./variables";
+import {body} from './variables.js';
+import showMainScreen from './main_screen.js'
 
 const wrapper = document.createElement('div');
 wrapper.classList.add('wrapper');
 
 body.insertAdjacentElement('afterbegin', wrapper);
+
+body.onload = () => {
+    showMainScreen();
+};
