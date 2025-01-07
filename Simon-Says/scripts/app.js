0a21f4c1151e14f20eee321262bcff2eaa5371ac
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2025-01-07 14:24:25 +0100

    refactor: add wrapper

---
+++ Simon-Says/scripts/app.js
@@ -0,0 +1,6 @@
+const body = document.getElementById('top');
+
+const wrapper = document.createElement('div');
+wrapper.classList.add('wrapper');
+
+body.insertAdjacentElement('afterbegin', wrapper);
