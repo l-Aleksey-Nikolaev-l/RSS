d85162d5491802b07acdd48d4555e9f2dd87a978
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2025-01-07 15:30:22 +0100

    refactor: add top section to MainScreen class for main screen

--- Simon-Says/scripts/main_screen.js
+++ Simon-Says/scripts/main_screen.js
@@ -1,4 +1,3 @@
-import {wrapper} from './variables.js';
 class MainScreen {
 
     #buttons = ['Play', 'Settings', 'Rules'];
@@ -7,6 +6,16 @@ class MainScreen {
         const main = document.createElement('main');
         main.classList.add('main');
         return main;
+    }
+
+    #topSection() {
+        const topSection = document.createElement('section');
+        topSection.classList.add('main__top_section');
+        const headerText = document.createElement('h1');
+        headerText.classList.add('main__header_text');
+        headerText.textContent = 'Simon Says Game'
+        topSection.append(headerText);
+        return topSection;
     }
 }
 
