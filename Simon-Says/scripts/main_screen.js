1a4b0b33cc8871f3ad42195ce7205f0ff79a534c
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2025-01-07 15:29:19 +0100

    refactor: add MainScreen class with main section

--- Simon-Says/scripts/main_screen.js
+++ Simon-Says/scripts/main_screen.js
@@ -1,4 +1,14 @@
 import {wrapper} from './variables.js';
+class MainScreen {
+
+    #buttons = ['Play', 'Settings', 'Rules'];
+
+    #mainSection() {
+        const main = document.createElement('main');
+        main.classList.add('main');
+        return main;
+    }
+}
 
 function showMainScreen() {
     return 0;
