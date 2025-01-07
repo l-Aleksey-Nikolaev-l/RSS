3291a9473ecd7960f903d0d3d4675e1cee15b0cd
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2025-01-07 18:56:21 +0100

    feat: import and implement screens by buttons.

--- Simon-Says/scripts/main_buttons.js
+++ Simon-Says/scripts/main_buttons.js
@@ -1,9 +1,19 @@
+import showPlayArea from './play_area_screen.js';
 import showSettings from './settings_screen.js';
+import showRules from './rules_screen.js';
 
 function clickMainButton(event) {
     const buttonName = event.target.textContent.toLowerCase();
-    if (buttonName === 'settings') {
-        showSettings();
+    switch (buttonName) {
+        case 'play':
+            showPlayArea();
+            break;
+        case 'settings':
+            showSettings();
+            break;
+        case 'rules':
+            showRules();
+            break;
     }
 }
 
