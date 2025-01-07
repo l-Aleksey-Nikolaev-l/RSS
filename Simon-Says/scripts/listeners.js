90755778a45af20e04641572c5edeeeb6415e128
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2025-01-07 19:11:34 +0100

    feat: add clearScreen function to main buttons listeners

--- Simon-Says/scripts/listeners.js
+++ Simon-Says/scripts/listeners.js
@@ -1,9 +1,11 @@
 import clickMainButton from './main_buttons.js';
+import clearScreen from './clear_screen.js';
 
 function startListeners() {
     const mainButtons = document.querySelectorAll('.main__button');
     mainButtons.forEach((button) => {
         button.addEventListener('click', (event) => {
+            clearScreen();
             clickMainButton(event);
         })
     });
