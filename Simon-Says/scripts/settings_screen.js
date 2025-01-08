a8aa36767569eb66442edd385d70742aacb97740
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2025-01-08 16:52:27 +0100

    refactor: add createMainSection function to class Settings

--- Simon-Says/scripts/settings_screen.js
+++ Simon-Says/scripts/settings_screen.js
@@ -1,6 +1,12 @@
 class Settings {
 
 }
+    #createMainSection() {
+        const main = document.createElement('main');
+        main.classList.add('main', 'main__settings_section');
+        return main;
+    }
+
 
 function showSettings() {
     console.log('Snow Settings');
