6243fe5c014ec0fcda6281c3ff6b24d54a89dd07
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2025-01-08 21:44:12 +0100

    refactor: add createTopSection function to PlayScreen class

--- Simon-Says/scripts/play_area_screen.js
+++ Simon-Says/scripts/play_area_screen.js
@@ -12,6 +12,33 @@ class PlayScreen {
         return main;
     }
 
+    #createTopSection() {
+        const topSection = document.createElement('section');
+        const attemptSection = document.createElement('div');
+        const roundSection = document.createElement('div');
+        const attemptText = document.createElement('p');
+        const attemptNumber = document.createElement('p');
+        const roundText = document.createElement('p');
+        const roundNumber = document.createElement('p');
+
+        topSection.classList.add('top__play_section');
+        attemptSection.classList.add('top__indicator', 'play__attempt');
+        roundSection.classList.add('top__indicator', 'play__round');
+        attemptText.classList.add('attempt__text');
+        attemptText.textContent = 'Attempt left:';
+        attemptNumber.classList.add('attempt__number');
+        attemptNumber.textContent = '1';
+        roundText.classList.add('round__text');
+        roundText.textContent = 'Round:';
+        roundNumber.classList.add('round__number');
+        roundNumber.textContent = '1/5';
+
+        attemptSection.append(attemptText, attemptNumber);
+        roundSection.append(roundText, roundNumber);
+        topSection.append(attemptSection, roundSection);
+        return topSection;
+    }
+
     #createControlButton(button = '') {
         const controlButton = document.createElement('button');
         controlButton.classList.add('main__button', `control__button_${button.toLowerCase()}`);
