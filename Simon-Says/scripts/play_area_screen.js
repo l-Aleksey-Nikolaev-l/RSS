17d68458aafc69b8613770ebeb165eecc053cf57
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2025-01-08 19:39:09 +0100

    refactor: add PlayScreen class

--- Simon-Says/scripts/play_area_screen.js
+++ Simon-Says/scripts/play_area_screen.js
@@ -1,5 +1,13 @@
 import {wrapper, difficulty} from "./variables";
 
+class PlayScreen {
+
+    constructor(difficulty) {
+        this.difficulty = difficulty;
+    }
+
+}
+
 function showPlayArea() {
     console.log('Play The Game');
 }
