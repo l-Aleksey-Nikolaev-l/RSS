5c53345cb06646fa858caa7993c5026e074c74d2
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2025-01-08 12:15:16 +0100

    refactor: change names of all class functions

--- Simon-Says/scripts/rules_screen.js
+++ Simon-Says/scripts/rules_screen.js
@@ -24,47 +24,47 @@ class Rules {
         ]
     ]
 
-    #mainSection() {
+    #createMainSection() {
         const main = document.createElement('main');
         main.classList.add('main', 'main__rules_section');
         return main;
     }
 
-    #addRulesList(rulesArray) {
+    #createRulesList(rulesArray) {
         const rulesList = document.createElement('ul');
         rulesList.classList.add('rules__list');
         for (let index = 0; index < rulesArray.length; index += 1) {
             if (typeof rulesArray[index] !== 'string') {
-                const subList = this.#addRulesList(rulesArray[index]);
+                const subList = this.#createRulesList(rulesArray[index]);
                 subList.classList.add('rules__list_sublist')
                 rulesList.append(subList);
             } else {
                 const rule = rulesArray[index];
-                const ruleItem = this.#addRule(rule, index + 1);
+                const ruleItem = this.#createRuleItem(rule, index + 1);
                 rulesList.append(ruleItem);
             }
         }
         return rulesList;
     }
 
-    #addRule(rule = '', index = 0) {
+    #createRuleItem(rule = '', index = 0) {
         const ruleItem = document.createElement('li');
         ruleItem.classList.add('rule__item', `rule__item_${index}`);
         ruleItem.textContent = rule;
         return ruleItem;
     }
 
-    #addBackButton() {
+    #createBackButton() {
         const backButton = document.createElement('button');
         backButton.classList.add('main__button');
         backButton.textContent = '< Back';
         return backButton;
     }
 
-    show() {
-        const mainSection = this.#mainSection();
-        const ruleSection = this.#addRulesList(this.#rulesArray);
-        const backButton = this.#addBackButton();
+    createRules() {
+        const mainSection = this.#createMainSection();
+        const ruleSection = this.#createRulesList(this.rulesArray);
+        const backButton = this.#createBackButton();
         mainSection.append(ruleSection, backButton);
         return mainSection;
     }
