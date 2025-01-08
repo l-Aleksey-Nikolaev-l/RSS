483331997447b84c1e9bd02370018e6c9eacbf0b
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2025-01-08 18:29:47 +0100

    refactor: add listener for setting difficulty

--- Simon-Says/scripts/listeners.js
+++ Simon-Says/scripts/listeners.js
@@ -1,14 +1,22 @@
 import clickMainButton from './main_buttons.js';
 import clearScreen from './clear_screen.js';
+import {setDifficulty} from './variables.js';
 
 function startListeners() {
     const mainButtons = document.querySelectorAll('.main__button');
+    const difficultySetting = document.getElementById('difficulty__list');
+
     mainButtons.forEach((button) => {
         button.addEventListener('click', (event) => {
             clearScreen();
             clickMainButton(event);
         })
     });
+
+    difficultySetting?.addEventListener('change', (event) => {
+        const level = event.target.value;
+        setDifficulty(level);
+    })
 }
 
 export default startListeners;
