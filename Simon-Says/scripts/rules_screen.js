5648a10608c3ef45f16a23d35fc1e876a4b693c4
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2025-01-08 12:31:46 +0100

    fix: add rules to array

--- Simon-Says/scripts/rules_screen.js
+++ Simon-Says/scripts/rules_screen.js
@@ -8,7 +8,26 @@ const rulesArray = [
     ['Easy: only numbers (0-9);',
         'Medium: only letters (A-Z);',
         'Hard: letters (A-Z) and numbers (0-9).'
-    ]
+    ],
+    'The same symbol can be used multiple times in a sequence.',
+    'For the letters, both upper and lower case letters are allowed and are ' +
+    'treated as the same symbol (e.g. if the sequence has the letter "a", it is ' +
+    'also possible to type "A")',
+    'The first round starts with 2 symbols based on the level of difficulty.',
+    'Each new round presents a new randomly generated sequence based on the level ' +
+    'of difficulty, with the sequence length increasing by two symbols each round ' +
+    '(culminating in a 10-symbol sequence by the 5th round).',
+    'The user can use both virtual (by clicking keys on the screen) and physical ' +
+    'keyboards (by pressing keys on their keyboard) to play the game.',
+    'Handling NumPad events is not required. NumPad inputs can be ignored during ' +
+    'the game.',
+    'Pressing keys with symbols that are not part of the current difficulty level ' +
+    'must be ignored during the game (e.g. for the "Medium" level, pressing any ' +
+    'not letter key must be ignored).',
+    'Only one key can be processed at a time. If the user attempts to press ' +
+    'multiple keys simultaneously, the application should process only the first ' +
+    'key press detected to prevent multiple inputs from being registered at the ' +
+    'same moment.'
 ]
 
 class Rules {
