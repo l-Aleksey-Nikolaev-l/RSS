fe25fa58e7f54155dc50d4cee07498d190149186
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2025-01-10 14:33:28 +0100

    refactor: add listener for physical keyboard pressed keys to game logic

--- Simon-Says/scripts/game_logic.js
+++ Simon-Says/scripts/game_logic.js
@@ -1,9 +1,31 @@
+let keyboardKey = null;
+
 function startKeysListeners() {
     const keyboard = document.getElementById('keyboard');
     let isMouseDown = false;
     let isKeyDown = false;
 
+    document.addEventListener('keydown', (event) => {
+        if (!isMouseDown && !isKeyDown) {
+            isKeyDown = true;
+            pressKey(keyboard, event.code);
+        }
+    });
 
+
+}
+
+function pressKey(keyboard, code) {
+    if (keyboardKey) {
+        return;
+    }
+    for (const key of keyboard.children) {
+        if (code === key.dataset.key) {
+            key.classList.add('key__pressed');
+            keyboardKey = code;
+            break;
+        }
+    }
 }
 
 
