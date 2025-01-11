cb2bdaf0aa4f8ae309b994e4486d661f3bd7d70c
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2025-01-11 16:08:28 +0100

    refactor: add global Map of keys and it's function

--- Simon-Says/scripts/variables.js
+++ Simon-Says/scripts/variables.js
@@ -2,11 +2,18 @@ const body = document.getElementById('top');
 const wrapper = document.getElementsByClassName('wrapper');
 
 let difficulty = 'easy';
+let keysCollection = new Map();
+
+function addKeyToCollection(key) {
+    keysCollection.set(key.dataset.key, key);
+}
 
 export {
     body,
     wrapper,
-    difficulty
+    difficulty,
+    keysCollection,
+    addKeyToCollection
 };
 
 export function setDifficulty(level) {
