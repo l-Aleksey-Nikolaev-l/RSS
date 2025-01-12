6a54fddf72084695ecc13c4dabf213f4ac87b970
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2025-01-12 23:06:29 +0100

    refactor: add listeners to listeners.js for changing volume of background music and effects throw settings screen

--- Simon-Says/scripts/listeners.js
+++ Simon-Says/scripts/listeners.js
@@ -14,6 +14,8 @@ function startListeners() {
     const mainButtons = document.querySelectorAll('.main__button');
     const difficultySetting = document.getElementById('difficulty__list');
     const playSetting = document.getElementById('music__play');
+    const musicSetting = document.getElementById('audio__setting');
+    const effectSetting = document.getElementById('effect__setting');
 
     mainButtons.forEach((button) => {
         button.addEventListener('click', (event) => {
@@ -31,6 +33,17 @@ function startListeners() {
         const state = event.target.checked;
         setMusicPlay(state);
         playBackgroundAudio(state);
+    });
+
+    musicSetting?.addEventListener('input', (event) => {
+        const level = event.target.value;
+        setMusicVolume(level);
+        setBackgroundVolume();
+    });
+
+    effectSetting?.addEventListener('input', (event) => {
+        const level = event.target.value;
+        setEffectsVolume(level);
     });
 }
 
