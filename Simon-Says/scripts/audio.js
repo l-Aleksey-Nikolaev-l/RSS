68beeb5616b338abcd512c5879675eaa80eefa08
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2025-01-12 11:32:00 +0100

    refactor: add function playBackgroundAudio to audio.js

--- Simon-Says/scripts/audio.js
+++ Simon-Says/scripts/audio.js
@@ -1,13 +1,22 @@
 import {backgroundVolume} from './variables.js';
 
+let audioFile = '';
+const audioPlayer = new Audio(audioFile);
+audioPlayer.loop = false;
+audioPlayer.volume = 0;
 
-const backgroundAudio = './assets/sounds/bg_sound.mp3';
-const backgroundPlayer = new Audio(backgroundAudio);
-backgroundPlayer.loop = true;
-backgroundPlayer.volume = backgroundVolume;
+function playBackgroundAudio() {
+    const randomNumber = Math.ceil(Math.random() * 3);
+    audioFile = `./assets/sounds/background/bg_sound_${randomNumber}.mp3`;
+    audioPlayer.loop = true;
+    audioPlayer.src = audioFile;
+    audioPlayer.volume = backgroundVolume;
+    return audioPlayer;
+}
 
 
 
 export {
     backgroundPlayer
+    playBackgroundAudio,
 }
