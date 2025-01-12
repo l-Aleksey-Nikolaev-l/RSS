66de042824034b425bdbd60680e2963d7d68acc6
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2025-01-12 23:17:16 +0100

    refactor: add own player for sound effects to audio.js

--- Simon-Says/scripts/audio.js
+++ Simon-Says/scripts/audio.js
@@ -3,7 +3,10 @@ import {backgroundVolume, effectsVolume} from './variables.js';
 const audioPlayer = new Audio();
 audioPlayer.loop = true;
 
-function playBackgroundAudio() {
+const effectPlayer = new Audio();
+effectPlayer.loop = false;
+
+function playBackgroundAudio(state) {
     const randomNumber = Math.ceil(Math.random() * 3);
     audioPlayer.src = `./assets/sounds/background/bg_sound_${randomNumber}.mp3`;
     audioPlayer.volume = backgroundVolume;
@@ -20,27 +23,25 @@ function setBackgroundVolume() {
 
 function playKeyEffectAudio(isCorrectKey) {
     if (isCorrectKey) {
-        audioFile = './assets/sounds/success/correct_key.wav';
+        effectPlayer.src = './assets/sounds/success/correct_key.wav';
     } else {
-        audioFile = './assets/sounds/fail/fail_key.wav';
+        effectPlayer.src = './assets/sounds/fail/fail_key.wav';
     }
-    audioPlayer.src = audioFile;
-    audioPlayer.volume = effectsVolume;
-    audioPlayer.play().then();
+    effectPlayer.volume = effectsVolume;
+    effectPlayer.play().then();
 }
 
 function playGameOverAudio(state) {
     if (state === 'round_won') {
-        audioFile = './assets/sounds/success/round_won.wav';
+        effectPlayer.src = './assets/sounds/success/round_won.wav';
     } else if (state === 'game_won') {
-        audioFile = './assets/sounds/success/game_won.wav';
+        effectPlayer.src = './assets/sounds/success/game_won.wav';
     }
     else if (state === 'game_lose') {
-        audioFile = './assets/sounds/fail/game_lose.wav';
+        effectPlayer.src = './assets/sounds/fail/game_lose.wav';
     }
-    audioPlayer.src = audioFile;
-    audioPlayer.volume = effectsVolume;
-    audioPlayer.play().then();
+    effectPlayer.volume = effectsVolume;
+    effectPlayer.play().then();
 }
 
 export {
