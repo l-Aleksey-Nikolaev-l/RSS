d5fbf4c2aad26ce0b6e25072efea760be906bac8
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2025-01-12 05:00:12 +0100

    refactor: replace code in createSequence to function from helpers.js

--- Simon-Says/scripts/game_logic.js
+++ Simon-Says/scripts/game_logic.js
@@ -1,5 +1,5 @@
+import {createKeysArray} from './heplers.js';
 import {
-    difficulty,
     keysCollection,
     timeForShowing,
     timeBetweenShows,
@@ -161,19 +161,10 @@ function keyBlink(key, answer) {
 }
 
 function createSequence() {
-    const numeric = [48, 57]; // from 0 to 9
-    const letters = [65, 90]; // from A to Z
-    let keysArray = [];
     resetAllVariables();
     resetValues();
 
-    if (difficulty === 'easy') {
-        keysArray.push(numeric);
-    } else if (difficulty === 'medium') {
-        keysArray.push(letters);
-    } else if (difficulty === 'hard') {
-        keysArray.push(numeric, letters);
-    }
+    let keysArray = createKeysArray();
 
     keysArray.forEach((pack) => {
         for (let code = pack[0]; code <= pack[1]; code += 1) {
