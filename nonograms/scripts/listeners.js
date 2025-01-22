452999659d68c56663baecd60e428c0a5bfdd79d
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2025-01-22 23:14:18 +0100

    feat: add to listeners.js possibility for cell to be filled, crossed or undo changes by mouse buttons

---
+++ nonograms/scripts/listeners.js
@@ -0,0 +1,28 @@
+const table = document.getElementsByClassName('nonograms__table');
+
+function tableListeners() {
+  table[0].addEventListener('mousedown', (event) => {
+    setStatus(event);
+  });
+
+  table[0].addEventListener('contextmenu', (event) => {
+    event.preventDefault();
+    setStatus(event);
+  });
+
+function setStatus(event) {
+  const cell = event.target;
+  const cellId = cell.dataset.id;
+  const mouseButton = event.type;
+  if (cellId && mouseButton === 'mousedown') {
+    cell.classList.toggle('fill');
+    cell.classList.remove('cross');
+  } else if (cellId && mouseButton === 'contextmenu') {
+    cell.classList.toggle('cross');
+    cell.classList.remove('fill');
+  }
+}
+
+export {
+  tableListeners
+};
