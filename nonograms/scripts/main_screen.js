4a44c9ef50ca03ea7850cb013e4fb26db6f60324
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2025-01-22 18:34:51 +0100

    refactor: add class MainScreen with basic components to main_screen.js and export it

--- nonograms/scripts/main_screen.js
+++ nonograms/scripts/main_screen.js
@@ -1,1 +1,40 @@
-console.log('main_screen');
+class MainScreen {
+  createMainContainer() {
+    const main = document.createElement('main');
+    main.classList.add('main');
+    return main;
+  }
+
+  createHeaderSection() {
+    const header = document.createElement('header');
+    header.classList.add('header');
+    return header;
+  }
+
+  createGameSection() {
+    const gameSection = document.createElement('section');
+    gameSection.classList.add('game__section');
+    return gameSection;
+  }
+
+  createFooterSection() {
+    const footer = document.createElement('footer');
+    footer.classList.add('footer');
+    return footer;
+  }
+
+  createMainScreen() {
+    const mainContainer = this.createMainContainer();
+    const header = this.createHeaderSection();
+    const gameSection = this.createGameSection();
+    const footer = this.createFooterSection();
+    mainContainer.append(header, gameSection, footer);
+    return mainContainer;
+  }
+}
+
+const mainScreen = new MainScreen().createMainScreen();
+
+export {
+  mainScreen
+};
