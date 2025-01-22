9c65259b18baaf23ceabd9e1bea78d84369e9c35
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2025-01-23 00:34:10 +0100

    refactor: add import tableListeners method to app.js

--- nonograms/scripts/app.js
+++ nonograms/scripts/app.js
@@ -1,6 +1,8 @@
 import { mainScreen } from './main_screen.js';
+import { tableListeners } from './listeners.js';
 
 const body = document.getElementsByClassName('body')[0];
 body.append(mainScreen);
 
+tableListeners();
 console.log('Ready!');
