2ff82b172b01638d71fc27a2f90320660d679bcd
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2025-01-23 22:43:31 +0100

    refactor: add objects with difficulty levels and their pictures to variables.js

--- nonograms/scripts/variables.js
+++ nonograms/scripts/variables.js
@@ -1,1 +1,34 @@
-console.log('variables');
+const difficultySettings = {
+  0: 'Easy (5x5)',
+  1: 'Medium (10x10)',
+  2: 'Hard (15x15)'
+};
+
+const picturesNames = {
+  0: {
+    0: 'Heart',
+    1: 'Smile',
+    2: 'Arrow',
+    3: 'House',
+    4: 'TV'
+  },
+  1: {
+    0: 'TV medium 1',
+    1: 'TV medium 2',
+    2: 'TV medium 3',
+    3: 'TV medium 4',
+    4: 'TV medium 5'
+  },
+  2: {
+    0: 'TV hard 1',
+    1: 'TV hard 2',
+    2: 'TV hard 3',
+    3: 'TV hard 4',
+    4: 'TV hard 5'
+  }
+};
+
+export {
+  difficultySettings,
+  picturesNames,
+};
