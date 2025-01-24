01087a1bf431afb60d0c8f9afbf057cb70017d0c
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2025-01-24 17:55:33 +0100

    fix: change import name in app.js

--- nonograms/scripts/app.js
+++ nonograms/scripts/app.js
@@ -1,9 +1,9 @@
 import { MainScreen } from './main_screen.js';
-import { tableListeners } from './listeners.js';
+import { gameListeners } from './listeners.js';
 
 const body = document.getElementsByClassName('body')[0];
 const mainScreen = new MainScreen().createMainScreen();
 body.append(mainScreen);
 
-tableListeners();
 console.log('Ready!');
+gameListeners();
