98006f5108b42e6f72e14b04f0c9082d4fe19bfd
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2025-01-25 19:03:07 +0100

    refactor: add functions for adding answers to and remove them from answerArray in game_logic.js

--- nonograms/scripts/game_logic.js
+++ nonograms/scripts/game_logic.js
@@ -3,10 +3,24 @@ import { pictures } from './pictures.js';
 
 function manageCell(targetCell) {
   const isCellFilled = targetCell.classList.contains('fill');
+  const targetCellRow = Number(targetCell.dataset.row);
+  const targetCellCol = Number(targetCell.dataset.col);
   if (isCellFilled) {
+    addAnswer(targetCellCol, targetCellRow);
   } else {
+    removeAnswer(targetCellCol, targetCellRow);
   }
   checkWinnings();
+}
+
+function addAnswer(targetCellCol, targetCellRow) {
+  answerArray.col_answer[targetCellCol][targetCellRow] += 1;
+  answerArray.row_answer[targetCellRow][targetCellCol] += 1;
+}
+
+function removeAnswer(targetCellCol, targetCellRow) {
+  answerArray.col_answer[targetCellCol][targetCellRow] -= 1;
+  answerArray.row_answer[targetCellRow][targetCellCol] -= 1;
 }
 
 function checkWinnings() {
