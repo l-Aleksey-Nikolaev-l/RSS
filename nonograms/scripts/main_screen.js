9f1e2a19ec2b74d3e35ad32c77498b73dd9e6cd1
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2025-01-25 23:41:16 +0100

    fix: add new logic of creating levels menu structure to createLevelsList method in main_screen.js

--- nonograms/scripts/main_screen.js
+++ nonograms/scripts/main_screen.js
@@ -29,11 +29,44 @@ class MainScreen {
     const listText = document.createElement('span');
     listText.classList.add('level__menu_text');
     listText.textContent = 'Levels ▼';
-    listItem.append(listText);
+    const levels = this.#createLevelsList(picsObject);
+    listItem.append(listText, levels);
+    listBlock.append(listItem);
     return listBlock;
   }
 
   #createLevelsList(picsObject) {
+    const levelsList = document.createElement('ul');
+    levelsList.classList.add('level__items');
+    const levelsNames = new Map();
+    const picturesNames = new Map();
+    let picturesCount = 0;
+
+    for (const index in picsObject) {
+      levelsNames.set(picsObject[index].level, '');
+      picturesNames.set(picsObject[index].name, picsObject[index].level);
+    }
+
+    levelsNames.forEach((_, levelName) => {
+      const levelItem = document.createElement('li');
+      levelItem.classList.add('level__item');
+      levelItem.textContent = levelName;
+      const imagesList = document.createElement('ul');
+      imagesList.classList.add('level__images');
+      levelsList.append(levelItem);
+      picturesNames.forEach((picLevel, picName) => {
+        if (picLevel === levelName) {
+          const picItem = document.createElement('li');
+          picItem.classList.add('level__image');
+          picItem.setAttribute('data-level-id', String(picturesCount));
+          picturesCount += 1;
+          picItem.textContent = picName;
+          imagesList.append(picItem);
+        }
+        levelItem.append(imagesList);
+      });
+    });
+    return levelsList;
   }
 
   #createGameSection() {
