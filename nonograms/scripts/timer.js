0ca9ca5fb593c8ada01e19134092aaee847549c3
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2025-01-26 02:40:57 +0100

    refactor: add interval and it's controls to timer.js

--- nonograms/scripts/timer.js
+++ nonograms/scripts/timer.js
@@ -1,11 +1,20 @@
+let timer = null;
+let isStarted = false;
 
 function startTimer() {
+  if (isStarted) {
+    return;
+  }
+  isStarted = true;
+  timer = setInterval(showTime, 1000);
 }
 
 function stopTimer() {
+  clearInterval(timer);
 }
 
 function resetTimer() {
+  isStarted = false;
 }
 
 function showTime() {
