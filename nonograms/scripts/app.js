c4e6e02d0fdae6cec88da9e568cc5b323353ba15
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2025-01-28 00:56:44 +0100

    fix: add import to app.js

--- nonograms/scripts/app.js
+++ nonograms/scripts/app.js
@@ -1,8 +1,9 @@
 import { MainScreen } from './main_screen.js';
 import { gameListeners } from './listeners.js';
+import { mainScreenParams } from './variables.js';
 
 const body = document.getElementsByClassName('body')[0];
-const mainScreen = new MainScreen().createMainScreen();
+const mainScreen = new MainScreen(mainScreenParams()).createMainScreen();
 body.append(mainScreen);
 
 gameListeners();
