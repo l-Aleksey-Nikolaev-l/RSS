374f816fe8275a013d7e61be7856a67e8f36fad1
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2025-01-30 23:58:30 +0100

    refactor: add Popup class with methods to popup.js

---
+++ nonograms/scripts/ui_components/popup.js
@@ -0,0 +1,39 @@
+import { winnerTime } from '../variables.js';
+
+class Popup {
+
+  constructor() {
+    this.beginText = 'Great! You have solved the nonogram in';
+    this.time = winnerTime;
+    this.endText = 'seconds!';
+  }
+
+  #createContainer(name) {
+    const main = document.createElement(name);
+    main.classList.add(`popup__${name}`);
+    return main;
+  }
+
+  createPopUp() {
+    const popupContainer = this.#createContainer('window');
+    const popupWrapper = this.#createContainer('wrapper');
+    const popupCloseButton = this.#createContainer('close_button');
+    const popupImage = this.#createContainer('image');
+    const popupCongrats = this.#createContainer('congrats');
+    const popupText = document.createElement('p');
+    popupText.classList.add('congrats__text');
+    popupText.textContent = `${this.beginText} ${this.time} ${this.endText}`;
+    const popupTime = document.createElement('span');
+    popupTime.classList.add('congrats__time');
+    popupText.append(popupTime);
+    popupCongrats.append(popupText);
+    popupWrapper.append(popupCloseButton, popupImage, popupCongrats);
+    popupContainer.append(popupWrapper);
+    return popupContainer;
+  }
+
+}
+
+export {
+  Popup
+};
