109bba06412080cbb22dc260884cb5cb2f24e621
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2025-01-30 01:54:28 +0100

    refactor: add fixed theme to body in app.js

--- nonograms/scripts/app.js
+++ nonograms/scripts/app.js
@@ -3,6 +3,7 @@ import { startListeners } from './listeners.js';
 import { mainScreenParams } from './variables.js';
 
 const body = document.getElementsByClassName('body')[0];
+body.classList.add('bg__light');
 const mainScreen = new MainScreen(mainScreenParams()).createMainScreen();
 body.append(mainScreen);
 
