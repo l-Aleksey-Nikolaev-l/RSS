68b2eea0ef2b022e2f9bc2de0e1e4413fe3bd967
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2025-01-31 20:47:51 +0100

    refactor: add saved data state to header.js and set button name based on it

--- nonograms/scripts/ui_components/header.js
+++ nonograms/scripts/ui_components/header.js
@@ -1,11 +1,13 @@
 import { LevelsMenu } from './levels_menu.js';
 import { Button } from './button.js';
+import { isConfigLoaded } from '../variables.js';
 
 class Header {
 
   constructor() {
     this.levelsMenu = new LevelsMenu();
     this.button = new Button();
+    this.isConfigLoaded = isConfigLoaded;
   }
 
   createHeaderSection() {
@@ -15,7 +17,8 @@ class Header {
     const settingsButton = this.button.createButton('settings', '');
     const repeatButton = this.button.createButton('repeat', 'Play again');
     const randomGameButton = this.button.createButton('random');
-    const saveButton = this.button.createButton('save', 'save');
+    const buttonName = this.isConfigLoaded ? 'resume' : 'save';
+    const saveButton = this.button.createButton(buttonName, buttonName);
 
     header.append(
       saveButton,
