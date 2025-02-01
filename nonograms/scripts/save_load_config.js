fe2e686f162c9f92a7bea9f7081460873fcce058
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2025-02-01 02:21:35 +0100

    refactor: add functions for saving and getting app config in save_load_config.js

--- nonograms/scripts/save_load_config.js
+++ nonograms/scripts/save_load_config.js
@@ -1,5 +1,9 @@
+import {
   getGameConfig,
   setGameConfig,
+  getAppConfig,
+  setAppConfig
+} from './variables.js';
 
 function saveCurrentGame() {
   const currentGameConfig = getGameConfig();
@@ -15,7 +19,23 @@ function getLastConfig() {
   }
 }
 
+function saveAppSettings() {
+  const currentAppConfig = getAppConfig();
+  const stringConfig = JSON.stringify(currentAppConfig);
+  localStorage.setItem('rss_nonograms_set', stringConfig);
+}
+
+function getAppSettings() {
+  let configData = localStorage.getItem('rss_nonograms_set');
+  configData = JSON.parse(configData);
+  if (configData) {
+    setAppConfig(configData);
+  }
+}
+
 export {
   saveCurrentGame,
-  getLastConfig
+  getLastConfig,
+  saveAppSettings,
+  getAppSettings
 };
