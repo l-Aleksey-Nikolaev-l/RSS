324bf08754bec7eda19bccde493e03f1bb82bdeb
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2025-02-02 01:15:23 +0100

    refactor: move functions for work with answers matrix to own module manage_matrix.js

--- nonograms/scripts/game_logic.js
+++ nonograms/scripts/game_logic.js
@@ -158,26 +158,6 @@ function manageCell(targetCell) {
   setAnswer(sign, targetCellCol, targetCellRow);
 }
 
-function addDataToMatrix(sign, targetCellCol, targetCellRow) {
-  if (sign.includes('fill')) {
-    vars.answerCells.col_answer[targetCellCol][targetCellRow] += 1;
-    vars.answerCells.row_answer[targetCellRow][targetCellCol] += 1;
-  } else if (sign.includes('cross')) {
-    vars.answerCells.col_cross[targetCellCol][targetCellRow] += 1;
-    vars.answerCells.row_cross[targetCellRow][targetCellCol] += 1;
-  }
-}
-
-function eraseDataFromMatrix(sign, targetCellCol, targetCellRow) {
-  if (sign.includes('fill')) {
-    vars.answerCells.col_answer[targetCellCol][targetCellRow] -= 1;
-    vars.answerCells.row_answer[targetCellRow][targetCellCol] -= 1;
-  } else if (sign.includes('cross')) {
-    vars.answerCells.col_cross[targetCellCol][targetCellRow] -= 1;
-    vars.answerCells.row_cross[targetCellRow][targetCellCol] -= 1;
-  }
-}
-
 function showPopUp() {
   timer.stopTimer(true);
   setBlock(true);
