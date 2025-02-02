b0c9e49409a886484a767484916ec638b5cf5a8a
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2025-02-02 05:48:14 +0100

    refactor: move project sources to src directory

--- nonograms/src/scripts/audio.js
+++ nonograms/src/scripts/audio.js
@@ -26,7 +26,7 @@ function setBackgroundVolume(value) {
 function playBackgroundAudio(state) {
   if (state && musicState) {
     musicPlayer.volume = musicVolume;
-    musicPlayer.src = './assets/sounds/bg_sound.mp3';
+    musicPlayer.src = '../assets/sounds/bg_sound.mp3';
     const playPromise = musicPlayer.play();
     if (playPromise !== undefined) {
       playPromise.then().catch(() => {});
@@ -40,15 +40,15 @@ function playCellEffectAudio(cellEvent) {
   const effectPlayer = new Audio();
   effectPlayer.loop = false;
   if (cellEvent === 'fill' && effectFillState) {
-    effectPlayer.src = './assets/sounds/fill.mp3';
+    effectPlayer.src = '../assets/sounds/fill.mp3';
     effectPlayer.volume = effectFillVolume;
     turnOnPlayer(effectPlayer);
   } else if (cellEvent === 'cross' && effectCrossState) {
-    effectPlayer.src = './assets/sounds/cross.mp3';
+    effectPlayer.src = '../assets/sounds/cross.mp3';
     effectPlayer.volume = effectCrossVolume;
     turnOnPlayer(effectPlayer);
   } else if (cellEvent === 'erase' && effectEraseState) {
-    effectPlayer.src = './assets/sounds/erase.mp3';
+    effectPlayer.src = '../assets/sounds/erase.mp3';
     effectPlayer.volume = effectEraseVolume;
     turnOnPlayer(effectPlayer);
   }
@@ -58,7 +58,7 @@ function playGameOverAudio() {
   const effectPlayer = new Audio();
   effectPlayer.loop = false;
   if (effectPopupState) {
-    effectPlayer.src = './assets/sounds/popup.mp3';
+    effectPlayer.src = '../assets/sounds/popup.mp3';
     effectPlayer.volume = effectPopupVolume;
     turnOnPlayer(effectPlayer);
   }
