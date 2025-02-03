3248841e25a9a9aeaf5714ee968805dfd27a53dd
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2025-02-03 11:05:02 +0100

    fix: change resumeGame function in resume_game.js

--- nonograms/src/scripts/helpers/resume_game.js
+++ nonograms/src/scripts/helpers/resume_game.js
@@ -1,7 +1,6 @@
 import * as slConfig from '../save_load_config.js';
 import * as audio from '../audio.js';
 import * as vars from '../variables.js';
-import { Puzzle } from '../puzzle_logic.js';
 import { setAnswer } from './manage_answer.js';
 import { fillTable } from './fill_table.js';
 import { setSaveResumeButton, setGameByLevel } from '../game_logic.js';
@@ -11,17 +10,10 @@ function resumeGame(event, table) {
   audio.setBlockAudio(true);
   setSaveResumeButton('save');
   const lastSave = vars.resumeLastGame().game.answerCells;
-  const colTips = lastSave.col_tips;
-  const rowTips = lastSave.row_tips;
-  const colCross = lastSave.col_cross;
-  const rowCross = lastSave.row_cross;
   setGameByLevel(event, vars.currentLevelId, table[0]);
-  const picArray = new Puzzle(colTips, rowTips).getPicByTips();
-  const crossArray = new Puzzle(colCross, rowCross).getPicByTips();
-  restoreData(['fill'], picArray);
-  restoreData(['cross'], crossArray);
-  fillTable(['fill'], picArray.flat());
-  fillTable(['cross'], crossArray.flat());
+  for (const sign in lastSave) {
+    restoreData([sign], lastSave[sign]);
+  }
 }
 
 function restoreData(sign, dataArray) {
