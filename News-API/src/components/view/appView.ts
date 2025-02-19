087eec81af225759ae214008f194817ef21d69fb
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2025-02-19 12:17:59 +0100

    refactor: add rough types to appView.ts

--- News-API/src/components/view/appView.ts
+++ News-API/src/components/view/appView.ts
@@ -2,17 +2,19 @@ import News from './news/news';
 import Sources from './sources/sources';
 
 export class AppView {
+    news: News;
+    sources: Sources;
     constructor() {
         this.news = new News();
         this.sources = new Sources();
     }
 
-    drawNews(data) {
+    drawNews(data: any): void {
         const values = data?.articles ? data?.articles : [];
         this.news.draw(values);
     }
 
-    drawSources(data) {
+    drawSources(data: any): void {
         const values = data?.sources ? data?.sources : [];
         this.sources.draw(values);
     }
