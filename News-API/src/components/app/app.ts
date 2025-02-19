c7a2e7f5d27f1041bd1be4505c388da9de1117aa
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2025-02-19 12:15:29 +0100

    refactor: add rough types to app.ts

--- News-API/src/components/app/app.ts
+++ News-API/src/components/app/app.ts
@@ -2,16 +2,26 @@ import AppController from '../controller/controller';
 import { AppView } from '../view/appView';
 
 class App {
+    private controller: AppController;
+    private view: AppView;
+
     constructor() {
         this.controller = new AppController();
         this.view = new AppView();
     }
 
-    start() {
-        document
-            .querySelector('.sources')
-            .addEventListener('click', (e) => this.controller.getNews(e, (data) => this.view.drawNews(data)));
-        this.controller.getSources((data) => this.view.drawSources(data));
+    public start(): void {
+        const sourcesButtons: HTMLElement = document.querySelector('.sources') as HTMLElement;
+
+        this.controller.getSources((data: any): void => {
+            this.view.drawSources(data);
+        });
+
+        sourcesButtons.addEventListener('click', (event: MouseEvent) => {
+            this.controller.getNews(event, (data: any): void => {
+                this.view.drawNews(data);
+            });
+        });
     }
 }
 
