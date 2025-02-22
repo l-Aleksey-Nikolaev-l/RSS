8c7477607e2c03e0660944207798674918820ede
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2025-02-22 13:58:22 +0100

    refactor: change type annotation in appLoader.ts

--- News-API/src/components/controller/appLoader.ts
+++ News-API/src/components/controller/appLoader.ts
@@ -2,8 +2,8 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super(process.env.API_URL as string, {
-            apiKey: process.env.API_KEY as string,
+        super(<string>process.env.API_URL, {
+            apiKey: <string>process.env.API_KEY,
         });
     }
 }
