c8be92118ad24c5b1945a8bef365bd2b4494af31
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2025-02-22 13:52:16 +0100

    refactor: change type annotation in controller.ts

--- News-API/src/components/controller/controller.ts
+++ News-API/src/components/controller/controller.ts
@@ -11,12 +11,12 @@ class AppController extends AppLoader {
     }
 
     getNews(event: MouseEvent, callback: any): void {
-        let target: HTMLElement = event.target as HTMLElement;
-        const newsContainer: HTMLElement = event.currentTarget as HTMLElement;
+        let target: HTMLElement = <HTMLElement>event.target;
+        const newsContainer: HTMLElement = <HTMLElement>event.currentTarget;
 
         while (target !== newsContainer) {
             if (target.classList.contains('source__item')) {
-                const sourceId: string = target.getAttribute('data-source-id') as string;
+                const sourceId: string = <string>target.getAttribute('data-source-id');
                 if (newsContainer.getAttribute('data-source') !== sourceId) {
                     newsContainer.setAttribute('data-source', sourceId);
                     super.getResp(
@@ -31,7 +31,7 @@ class AppController extends AppLoader {
                 }
                 return;
             }
-            target = target.parentNode as HTMLElement;
+            target = <HTMLElement>target.parentNode;
         }
     }
 }
