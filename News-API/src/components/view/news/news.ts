7c810367ebbf33d264daa5e4669bca9cbfb9fb3c
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2025-02-22 13:47:38 +0100

    refactor: change type annotation in news.ts

--- News-API/src/components/view/news/news.ts
+++ News-API/src/components/view/news/news.ts
@@ -11,20 +11,20 @@ class News {
             selectedNews = newsArray;
         }
 
-        const fragment: DocumentFragment = document.createDocumentFragment() as DocumentFragment;
-        const newsItemTemp: HTMLTemplateElement = document.querySelector('#newsItemTemp') as HTMLTemplateElement;
-        const newsSection: HTMLElement = document.querySelector('.news') as HTMLElement;
+        const fragment: DocumentFragment = <DocumentFragment>document.createDocumentFragment();
+        const newsItemTemp: HTMLTemplateElement = <HTMLTemplateElement>document.querySelector('#newsItemTemp');
+        const newsSection: HTMLElement = <HTMLElement>document.querySelector('.news');
 
         selectedNews.forEach((item: NewsType, index: number): void => {
-            const newsClone: HTMLTemplateElement = newsItemTemp.content.cloneNode(true) as HTMLTemplateElement;
-            const newsItem: HTMLElement = newsClone.querySelector('.news__item') as HTMLElement;
-            const newsImage: HTMLElement = newsClone.querySelector('.news__meta-photo') as HTMLElement;
-            const newsAuthor: HTMLElement = newsClone.querySelector('.news__meta-author') as HTMLElement;
-            const newsDate: HTMLElement = newsClone.querySelector('.news__meta-date') as HTMLElement;
-            const newsTitle: HTMLElement = newsClone.querySelector('.news__description-title') as HTMLElement;
-            const newsSource: HTMLElement = newsClone.querySelector('.news__description-source') as HTMLElement;
-            const newsDescription: HTMLElement = newsClone.querySelector('.news__description-content') as HTMLElement;
-            const newsLink: HTMLElement = newsClone.querySelector('.news__read-more a') as HTMLElement;
+            const newsClone: HTMLTemplateElement = <HTMLTemplateElement>newsItemTemp.content.cloneNode(true);
+            const newsItem: HTMLElement = <HTMLElement>newsClone.querySelector('.news__item');
+            const newsImage: HTMLElement = <HTMLElement>newsClone.querySelector('.news__meta-photo');
+            const newsAuthor: HTMLElement = <HTMLElement>newsClone.querySelector('.news__meta-author');
+            const newsDate: HTMLElement = <HTMLElement>newsClone.querySelector('.news__meta-date');
+            const newsTitle: HTMLElement = <HTMLElement>newsClone.querySelector('.news__description-title');
+            const newsSource: HTMLElement = <HTMLElement>newsClone.querySelector('.news__description-source');
+            const newsDescription: HTMLElement = <HTMLElement>newsClone.querySelector('.news__description-content');
+            const newsLink: HTMLElement = <HTMLElement>newsClone.querySelector('.news__read-more a');
 
             if (index % 2) {
                 newsItem.classList.add('alt');
