2a3421e47596b138651ce0d663b7d31cd08bd5a7
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2025-02-22 13:57:48 +0100

    refactor: add modifiers for fields and methods in appView.ts

--- News-API/src/components/view/appView.ts
+++ News-API/src/components/view/appView.ts
@@ -2,20 +2,20 @@ import News from './news/news';
 import Sources from './sources/sources';
 import { NewsType, ResponseObject, SourcesType } from '../../helpers/types';
 
-export class AppView {
-    news: News;
-    sources: Sources;
+class AppView {
+    private readonly news: News;
+    private readonly sources: Sources;
     constructor() {
         this.news = new News();
         this.sources = new Sources();
     }
 
-    drawNews(data: ResponseObject): void {
+    public drawNews(data: ResponseObject): void {
         const values: NewsType[] = data?.articles ? data?.articles : [];
         this.news.draw(values);
     }
 
-    drawSources(data: ResponseObject): void {
+    public drawSources(data: ResponseObject): void {
         const values: SourcesType[] = data?.sources ? data?.sources : [];
         this.sources.draw(values);
     }
