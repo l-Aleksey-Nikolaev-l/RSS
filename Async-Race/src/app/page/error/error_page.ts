f955f86cdf6d1d7ef461757589b195c5f62f9376
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2025-03-26 22:28:12 +0100

    refactor: add an ErrorPage class to error_page.ts

--- Async-Race/src/app/page/error/error_page.ts
+++ Async-Race/src/app/page/error/error_page.ts
@@ -1,1 +1,23 @@
 import './error_page.css';
+import { Page } from '../page';
+import { CreateElement } from '../../utility/create_element';
+import type { ElementParams } from '../../helper/types';
+
+class ErrorPage extends Page {
+    constructor() {
+        super();
+    }
+
+    public setComponent(): HTMLElement {
+        const params: ElementParams = {
+            tag: 'div',
+            classNames: ['error__page'],
+            callback: (): void => {},
+        };
+        const errorPage: HTMLElement = new CreateElement(params).getElement();
+        errorPage.textContent = 'ERROR PAGE!';
+        return errorPage;
+    }
+}
+
+export { ErrorPage };
