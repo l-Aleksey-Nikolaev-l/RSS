b3558983f486e1a2b588d1953f1039612b0612a9
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2025-03-26 22:29:16 +0100

    refactor: add a GaragePage class to garage_page.ts

--- Async-Race/src/app/page/garage/garage_page.ts
+++ Async-Race/src/app/page/garage/garage_page.ts
@@ -1,1 +1,23 @@
 import './garage_page.css';
+import { Page } from '../page';
+import { CreateElement } from '../../utility/create_element';
+import type { ElementParams } from '../../helper/types';
+
+class GaragePage extends Page {
+    constructor() {
+        super();
+    }
+
+    public setComponent(): HTMLElement {
+        const params: ElementParams = {
+            tag: 'div',
+            classNames: ['garage__page'],
+            callback: (): void => {},
+        };
+        const garagePage: HTMLElement = new CreateElement(params).getElement();
+        garagePage.textContent = 'GARAGE PAGE!';
+        return garagePage;
+    }
+}
+
+export { GaragePage };
