b1a4f698cbbce74bc4fe150dc4036abd8e816715
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2025-03-26 22:31:30 +0100

    refactor: add a WinnersPage class to winners_page.ts

--- Async-Race/src/app/page/winners/winners_page.ts
+++ Async-Race/src/app/page/winners/winners_page.ts
@@ -1,1 +1,23 @@
 import './winners_page.css';
+import { Page } from '../page';
+import { CreateElement } from '../../utility/create_element';
+import type { ElementParams } from '../../helper/types';
+
+class WinnersPage extends Page {
+    constructor() {
+        super();
+    }
+
+    public setComponent(): HTMLElement {
+        const params: ElementParams = {
+            tag: 'div',
+            classNames: ['winners__page'],
+            callback: (): void => {},
+        };
+        const winnersPage: HTMLElement = new CreateElement(params).getElement();
+        winnersPage.textContent = 'WINEEEEERS!';
+        return winnersPage;
+    }
+}
+
+export { WinnersPage };
