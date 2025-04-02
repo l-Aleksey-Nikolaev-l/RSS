4a45398afc24d0a50c85a5e7a8a2ce91a9706a67
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2025-04-02 23:17:29 +0200

    refactor: add buildCar method to CarBlock class in car_block.ts

--- Async-Race/src/app/utility/car_block/car_block.ts
+++ Async-Race/src/app/utility/car_block/car_block.ts
@@ -1,5 +1,23 @@
+import { CarBase } from './car_base';
+import { CarEngine } from './car_engine';
+
 class CarBlock {
     constructor() {}
+
+    public buildCar = async (): Promise<HTMLElement> => {
+        const oneCar: Promise<object> = new CarBase().getCarById(1);
+        const oneEng: Promise<object> = new CarEngine().getEngine(1);
+        return await oneCar.then(async (car: object): Promise<HTMLElement> => {
+            return await oneEng.then((eng: object): HTMLElement => {
+                const carContainer: HTMLElement = document.createElement('div');
+                const carParams: HTMLElement = document.createElement('div');
+                carParams.textContent += JSON.stringify(car);
+                carParams.textContent += JSON.stringify(eng);
+                carContainer.append(carParams);
+                return carContainer;
+            });
+        });
+    };
 }
 
 export { CarBlock };
