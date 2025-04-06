314a75c9565b7d5d75110c1409897ea927043bb5
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2025-04-06 18:16:38 +0200

    refactor: change buildCars method of CarBlock class in car_block.ts

--- Async-Race/src/app/utility/car_block/car_block.ts
+++ Async-Race/src/app/utility/car_block/car_block.ts
@@ -1,23 +1,32 @@
 import { CarBase } from './car_base';
 import { CarEngine } from './car_engine';
 
+type Engine = {
+    distance: number;
+    velocity: number;
+};
+
+type Car = {
+    name: string;
+    color: string;
+    id: number;
+};
+
 class CarBlock {
     constructor() {}
 
-    public buildCar = async (): Promise<HTMLElement> => {
-        const oneCar: Promise<object> = new CarBase().getCarById(1);
-        const oneEng: Promise<object> = new CarEngine().getEngine(1);
-        return await oneCar.then(async (car: object): Promise<HTMLElement> => {
-            return await oneEng.then((eng: object): HTMLElement => {
-                const carContainer: HTMLElement = document.createElement('div');
-                const carParams: HTMLElement = document.createElement('div');
-                carParams.textContent += JSON.stringify(car);
-                carParams.textContent += JSON.stringify(eng);
-                carContainer.append(carParams);
-                return carContainer;
+    public async buildCars(): Promise<HTMLElement> {
+        const carsContainer: HTMLElement = document.createElement('div');
+        carsContainer.classList.add('cars_container');
+        const allCars: Promise<object> = new CarBase().getAllCars();
+        return await allCars.then((cars: object) => {
+            Object.values(cars).forEach((carParams: Car) => {
+                const newCar: HTMLElement = this.createCar(carParams);
+                carsContainer.append(newCar);
             });
+            return carsContainer;
         });
-    };
+    }
 }
 
 export { CarBlock };
